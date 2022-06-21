52c415fff4dfface8e8dddaa97a68f9816fd3790
Ramiro <ramiro.nicolas.martinez14@gmail.com>
2022-06-21 15:35:54 -0300

    Programa indicador de mayor entre 2 numeros

--- Principal/src/Main.java
+++ Principal/src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int a, b;
 
-        System.out.println("Hello world!");
+        System.out.println("Ingrese un numero entero:");
+        a = read.nextInt();
+        System.out.println("Ingrese otro numero entero:");
+        b = read.nextInt();
+        if (a>b){
+            System.out.println("El numero "+a+" es mayor que el numero "+b);
+        }
+        else
+            System.out.println("El numero "+b+" es mayor que el numero "+a);
     }
 }
