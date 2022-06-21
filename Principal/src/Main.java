9ef1a7e8670c672ba85e0bc3f92dbf30908a9108
Ramiro <ramiro.nicolas.martinez14@gmail.com>
2022-06-21 19:05:18 -0300

    Ejercicio 2 de condicionales

--- Principal/src/Main.java
+++ Principal/src/Main.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
 
-        System.out.println("Hello world!");
+        String producto;
+        float precio;
+        byte met_pago;
+        char salida;
+
+        do {
+            System.out.println("Ingrese el nombre del producto:");
+            producto = read.next();
+            System.out.println("Ingrese el precio del producto:");
+            precio = read.nextFloat();
+            System.out.println("Ingrese el método de pago:\n\t1- Contado/Efectivo.\n\t2- Tarjeta de débito.\n\t3- Tarjeta de crédito.");
+            met_pago = read.nextByte();
+            while(met_pago != 1 && met_pago != 2 && met_pago != 3){
+                System.out.println("Método de pago erroneo.");
+                System.out.println("Ingrese el método de pago:\n\t1- Contado/Efectivo.\n\t2- Tarjeta de débito.\n\t3- Tarjeta de crédito.");
+                met_pago = read.nextByte();
+            }
+            switch (met_pago) {
+                case 1: precio *= 0.85;
+                        break;
+                case 3: precio *= 1.1;
+                        break;
+            }
+            System.out.println("Producto:"+producto+" - Precio:"+precio);
+            System.out.println("Desea ingresar otro producto? (S/N)");
+            salida = read.next().charAt(0);
+            salida = java.lang.Character.toUpperCase(salida);
+        }while (salida == 'S');
+
+
     }
 }
