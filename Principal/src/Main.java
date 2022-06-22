941c8f1ac19a4bbb2cb6bc647efdca7b28b5260e
Ramiro <ramiro.nicolas.martinez14@gmail.com>
2022-06-22 03:52:10 -0300

    Ejercicio 3c) iterativo

--- Principal/src/Main.java
+++ Principal/src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        /* for (int i = 8; i > 1; i--) {         //NO PIDE ESTO ↓ PERO ME COSTÓ Y ME GUSTÓ PODER HACERLO
+            for (int k = 0; k < 8 - i; k++) {    //*******
+                System.out.print(" ");           // ******
+            }                                    //  *****
+            for (int j = 1; j < i; j++) {        //   ****
+                System.out.print("*");           //    ***
+            }                                    //     **
+            System.out.println();                //      *
+        }*/
+
+        for (int i = 1; i < 8; i++) {
+            for (int j = 1; j < 8 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
     }
 }
