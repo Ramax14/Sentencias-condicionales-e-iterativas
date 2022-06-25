58968fc80102f552ea7bdfefa34cbac747ad1885
Ramiro Martínez <ramiro.nicolas.martinez14@gmail.com>
2022-06-24 21:42:21 -0300

    Imprime numeros del 1 al 10

--- Principal/src/Main.java
+++ Principal/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        CicloFor();
+    }
+
+    public static void CicloFor(){
         for (int i = 1; i < 11; i++) {
             System.out.println(i);
         }
