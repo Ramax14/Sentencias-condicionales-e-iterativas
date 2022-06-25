cc6a7dc82c20bbc53d65bd68cb1b18f52cc79154
Ramiro Martínez <ramiro.nicolas.martinez14@gmail.com>
2022-06-24 21:54:32 -0300

    Imprime un triangulo creciente con espacios a izquierda

--- Principal/src/Main.java
+++ Principal/src/Main.java
@@ -1,15 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        /* for (int i = 8; i > 1; i--) {         //NO PIDE ESTO ↓ PERO ME COSTÓ Y ME GUSTÓ PODER HACERLO
-            for (int k = 0; k < 8 - i; k++) {    //*******
-                System.out.print(" ");           // ******
-            }                                    //  *****
-            for (int j = 1; j < i; j++) {        //   ****
-                System.out.print("*");           //    ***
-            }                                    //     **
-            System.out.println();                //      *
-        }*/
+        /* for (int i = 8; i > 1; i--) {         NO PIDE ESTO ↓ PERO ME COSTÓ Y ME GUSTÓ PODER HACERLO
+            for (int k = 0; k < 8 - i; k++) {              *******
+                System.out.print(" ");                      ******
+            }                                                *****
+            for (int j = 1; j < i; j++) {                     ****
+                System.out.print("*");                         ***
+            }                                                   **
+            System.out.println();                                *
+           }
+         */
+
+        TrianguloCrecienteCEspacioAIzq();
+    }
+
+    public static void TrianguloCrecienteCEspacioAIzq(){
 
         for (int i = 1; i < 8; i++) {
             for (int j = 1; j < 8 - i; j++) {
@@ -20,6 +26,6 @@ public class Main {
             }
             System.out.println();
         }
-        
     }
+
 }
