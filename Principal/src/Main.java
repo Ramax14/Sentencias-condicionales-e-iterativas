318ced4becccf2df0a85d2afdfde1ad1d4e8d07a
Ramiro Martínez <ramiro.nicolas.martinez14@gmail.com>
2022-06-24 21:40:18 -0300

    Escribe 10 veces la palabra 'foo'

--- Principal/src/Main.java
+++ Principal/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        Pal_foo();
+    }
+
+    public static void Pal_foo(){
         String word = "foo";
 
         for (int i = 0; i < 10; i++) {
