47bf1d2c2d67c1ffa9a8d52d978a8b1f74b12fc6
Ramiro Martínez <ramiro.nicolas.martinez14@gmail.com>
2022-06-24 21:48:59 -0300

    Imprime por consola un triangulo decreciente

--- Principal/src/Main.java
+++ Principal/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        TrianguloDecreciente();
+    }
+
+    public static void TrianguloDecreciente(){
+
         for (int i = 7; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
